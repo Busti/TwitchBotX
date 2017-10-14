aaa199b17e29ae01c98e1df2ca3f38a9cf16fb99
busti <mbust@live.de>
2017-10-14 22:34:02 +0200

    Created chat analyzer tool.

--- src/main/scala/org/foilage/tbx/ChatListener.scala
+++ src/main/scala/org/foilage/tbx/ChatListener.scala
@@ -1,16 +1,45 @@
 package org.foilage.tbx
 
-import org.pircbotx.hooks.events.{BanListEvent, NoticeEvent, OutputEvent, UnknownEvent}
-import org.pircbotx.hooks.{Event, ListenerAdapter}
+import javafx.scene.paint.Paint
+
+import org.pircbotx.hooks.ListenerAdapter
 import org.pircbotx.hooks.types.GenericMessageEvent
 
-class ChatListener extends ListenerAdapter {
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+import scalafx.scene.canvas.GraphicsContext
+import scalafx.scene.paint.Color._
+import scalafx.scene.text.Font
+
+class ChatListener(size: Int) extends ListenerAdapter {
+  var words = ListBuffer[String]()
+
   override def onGenericMessage(event: GenericMessageEvent) {
-    //println(event.getMessage())
+    val msg = event.getMessage
+    words += msg//.split(' ')
+    if (words.size > size) {
+      val offset = words.size - size
+      for (i <- 0 until offset)
+        words.remove(0)
+    }
   }
 
-  override def onUnknown(event: UnknownEvent) {
+  def draw(gc: GraphicsContext) {
+    val data = words.toArray
 
-    println(event)
+    for ((w, i) <- data.zipWithIndex) {
+      val code = data(i).hashCode
+      gc.fill = rgb(code & 255, (code >> 8) & 255, (code >> 16) & 255)
+      gc.font = Font(6)
+      gc.fillText(w, 6 * 200, i * 6)
+    }
+
+    for (x <- data.indices; y <- data.indices) {
+      if (data(x) == data(y)) {
+        val code = data(x).hashCode
+        gc.fill = rgb(code & 255, (code >> 8) & 255, (code >> 16) & 255)
+        gc.fillRect(x * 6, y * 6, 6, 6)
+      }
+    }
   }
 }
--- src/main/scala/org/foilage/tbx/Main.scala
+++ src/main/scala/org/foilage/tbx/Main.scala
@@ -1,40 +1,68 @@
 package org.foilage.tbx
 
 import java.io.{File, FileInputStream}
-import java.nio.file.Files
 import java.util
 
 import org.pircbotx.{Configuration, PircBotX}
 import org.pircbotx.cap.EnableCapHandler
 import org.yaml.snakeyaml.Yaml
 
-object Main {
+import scalafx.Includes._
+import scalafx.animation.AnimationTimer
+import scalafx.application.JFXApp
+import scalafx.scene.Scene
+import scalafx.scene.canvas.Canvas
+import scalafx.scene.paint.Color._
+
+object Main extends JFXApp {
   var bot: PircBotX = _
 
-  def main(args: Array[String]) {
-    val yaml = new Yaml()
-    val cfg = yaml.load(new FileInputStream(new File("config.yml"))).asInstanceOf[util.Map[String, Any]]
-    val bot1 = cfg.get("bots").asInstanceOf[util.List[util.Map[String, String]]].get(0)
+  val yaml = new Yaml()
+  val cfg = yaml.load(new FileInputStream(new File("config.yml"))).asInstanceOf[util.Map[String, Any]]
+  val bot1 = cfg.get("bots").asInstanceOf[util.List[util.Map[String, String]]].get(0)
 
-    val config = new Configuration.Builder()
-      .setName(bot1.get("name"))
-      .setLogin(bot1.get("name"))
-      .setServerPassword("oauth:" + bot1.get("oauth"))
-      .addServer("irc.twitch.tv")
-      .addAutoJoinChannel("#gamesdonequick")
-      .setAutoNickChange(false)
-      .addCapHandler(new EnableCapHandler("twitch.tv/membership"))
-      .addCapHandler(new EnableCapHandler("twitch.tv/tags"))
-      .addCapHandler(new EnableCapHandler("twitch.tv/commands"))
-      .addListener(new ChatListener)
-      .setMessageDelay(1600)
-      .buildConfiguration()
+  val listener = new ChatListener(200)
 
-    bot = new PircBotX(config)
+  val config = new Configuration.Builder()
+    .setName(bot1.get("name"))
+    .setLogin(bot1.get("name"))
+    .setServerPassword("oauth:" + bot1.get("oauth"))
+    .addServer("irc.twitch.tv")
+    .addAutoJoinChannel("#forsenlol")
+    .setAutoNickChange(false)
+    .addCapHandler(new EnableCapHandler("twitch.tv/membership"))
+    .addCapHandler(new EnableCapHandler("twitch.tv/tags"))
+    .addCapHandler(new EnableCapHandler("twitch.tv/commands"))
+    .addListener(listener)
+    .setMessageDelay(1600)
+    .buildConfiguration()
 
-    new Thread(new ConsoleListener(bot)).start()
+  bot = new PircBotX(config)
 
-    bot.startBot()
+  new Thread(new ConsoleListener(bot)).start()
+
+  new Thread(new Runnable {
+    override def run() {
+      bot.startBot()
+    }
+  }).start()
+
+  stage = new JFXApp.PrimaryStage {
+    title.value = "Twitchgems"
+    width = 200
+    height = 200
+    scene = new Scene { _scene =>
+      content = new Canvas {
+        width <== _scene.width
+        height <== _scene.height
+        val timer = AnimationTimer { t =>
+          graphicsContext2D.fill = Black
+          graphicsContext2D.fillRect(0, 0, width.value, height.value)
+          listener.draw(graphicsContext2D)
+        }
+        timer.start()
+      }
+    }
   }
 }
 
@@ -47,6 +75,7 @@ class ConsoleListener(bot: PircBotX) extends Runnable {
       ln match {
         case "quit" =>
           bot.send().quitServer()
+          Main.stopApp()
           running = false
         case _ =>
       }
